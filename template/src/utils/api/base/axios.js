861aa6af5ab9378bd307071623ff1e5c0b80d50c
Constantinos Ntzoufras <cntzoufras@gmail.com>
2023-12-27 17:37:25 +0200

    Update axios request to local api

--- template/src/utils/api/base/axios.js
+++ template/src/utils/api/base/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { getToken } from '../../helpers';
 
-export const apiUrl = 'https://c-proxy.int.aspirity.com';
+// export const apiUrl = 'https://c-proxy.int.aspirity.com';
+export const apiUrl = 'http://localhost/api/';
 
 export const defaultParams = () => ({
   headers: { Authorization: `Bearer ${getToken()}` },
