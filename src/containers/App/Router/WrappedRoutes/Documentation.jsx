ddd053ec4fb3c0ebeb631f2e22c7f703a56d9a67
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-01-13 02:05:38 +0200

    Update Documentation in Authentication with Laravel

--- src/containers/App/Router/WrappedRoutes/Documentation.jsx
+++ src/containers/App/Router/WrappedRoutes/Documentation.jsx
@@ -8,7 +8,6 @@ import FinanceDashboardDoc from '../../../Documentation/05_finance_dashboard/ind
 import Form from '../../../Documentation/06_forms/index';
 import ColorThemes from '../../../Documentation/07_change_and_add_color_themes/index';
 import NavigationItem from '../../../Documentation/08_new_navigation_item/index';
-import Authentication from '../../../Documentation/09_authentication/index';
 import Resources from '../../../Documentation/10_resources/index';
 import Changelog from '../../../Documentation/11_changelog/index';
 import FAQ from '../../../Documentation/12_faq/index';
@@ -23,7 +22,6 @@ export default () => (
     <Route path="/documentation/form" component={Form} />
     <Route path="/documentation/color_themes" component={ColorThemes} />
     <Route path="/documentation/navigation_item" component={NavigationItem} />
-    <Route path="/documentation/authentication" component={Authentication} />
     <Route path="/documentation/resources" component={Resources} />
     <Route path="/documentation/changelog" component={Changelog} />
     <Route path="/documentation/faq" component={FAQ} />
