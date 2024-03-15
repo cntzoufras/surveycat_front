53dc54d1ec6e849ee77a66068efb3860b00720f9
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-03-15 18:26:57 +0200

    Fix translations

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,30 +1,32 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { Provider, useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { LoadScript } from '@react-google-maps/api';
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { LoadScript } from '@react-google-maps/api';
+import initAuth0 from '@/shared/components/account/auth/withAuth0';
 import TimepickerStyles from '@/shared/components/form/date-pickers/timepickerStyles';
 import Loading from '@/shared/components/Loading';
+
 import Router from './Router';
 import store from './store';
 import ScrollToTop from './ScrollToTop';
+import { config as i18nextConfig } from '../../translations';
 import GlobalStyles from './globalStyles';
 import RechartStyles from './rechartStyles';
 import NotificationStyles from './notificationStyles';
 import CalendarStyles from './calendarStyles';
 
-// Remove completely remove i18n & auth0 from initialization app.jsx file
+i18n.init(i18nextConfig);
 
-const ThemeComponent = ({
-  children,
-}) => {
+const ThemeComponent = ({ children }) => {
   const {
-    mode, direction, shadow, border,
-  } = useSelector(state => ({
+   mode, direction, shadow, border, 
+   } = useSelector(state => ({
     mode: state.theme.className,
     direction: state.rtl.direction,
     shadow: state.shadow.className,
@@ -64,22 +66,26 @@ ThemeComponent.propTypes = {
 
 const ConnectedThemeComponent = ThemeComponent;
 
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter basename="/">
-      <ConnectedThemeComponent>
-        <LoadScript
-          loadingElement={<Loading loading />}
-          googleMapsApiKey=""
-        >
-          <ScrollToTop>
-            <Router />
-          </ScrollToTop>
-        </LoadScript>
-      </ConnectedThemeComponent>
-    </BrowserRouter>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    window.addEventListener('load', initAuth0);
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter basename="/easydev">
+        <I18nextProvider i18n={i18n}>
+          <ConnectedThemeComponent>
+            <LoadScript loadingElement={<Loading loading />} googleMapsApiKey="">
+              <ScrollToTop>
+                <Router />
+              </ScrollToTop>
+            </LoadScript>
+          </ConnectedThemeComponent>
+        </I18nextProvider>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
-
