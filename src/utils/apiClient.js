ba55aae52d05066d0a51d90e139c44ad498f0275
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-05-03 19:17:38 +0300

    Fix errors on build

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import appConfigApi from './api/appConfigApi';
 import covidApi from './api/covidApi';
 import todoApi from './api/todoApi';
-import surveyApi from './api/surveyApi'
+import surveyApi from './api/surveyApi';
 import pokemonApi from './api/pokemonApi';
 
 const api = {
