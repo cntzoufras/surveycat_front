e84b854693b5fbd1e3f27b96a83cf0f2e74fc9d4
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-10 23:49:58 +0300

    Before update to handleLogin

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import api, { setAuthToken } from '@/utils/apiClient';
 
 export const AUTHENTICATE = 'AUTHENTICATE';
 export const AUTHENTICATE_ERROR_AUTH = 'AUTHENTICATE_ERROR_AUTH';
@@ -11,15 +10,14 @@ export const AUTHENTICATE_REGISTER_ERROR = 'AUTHENTICATE_REGISTER_ERROR';
 export function auth({ name, avatar, token }) {
   return {
     type: AUTHENTICATE,
-    token,
-    user: { name, avatar },
+    payload: { token },
   };
 }
 
-export function login({ fullName, avatar, token }) {
+export function login({  token }) {
   return {
     type: AUTHENTICATE_LOGIN,
-    payload: { fullName, avatar, token },
+    payload: { token },
   };
 }
 
@@ -34,56 +32,57 @@ export function logout() {
   return { type: AUTHENTICATE_LOGOUT };
 }
 
-export const handleLogout = () => (dispatch) => {
-  localStorage.clear();
-  sessionStorage.clear();
-  dispatch(logout());
+export const handleLogout = () => async (dispatch) => {
+  try {
+    const auth = JSON.parse(localStorage.getItem('auth'));
+    console.log('auth:', auth);
+    const token = auth ? auth.token : null;
+
+    if (!token) {
+      throw new Error('No auth token found');
+    }
+
+    await axios.post(`${process.env.REACT_APP_BASE_URL}/logout`, {}, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept':'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      withCredentials: true,
+      withXSRFToken:true
+    });
+
+    dispatch(logout());
+    localStorage.removeItem('auth');
+  } catch (error) {
+    console.error('Logout failed:', error);
+  }
 };
 
 export const handleLogin = credentials => async (dispatch) => {
   try {
-    const updatedUrl = process.env.REACT_APP_API_URL.replace(/\/api/, '');
-        
-    // Fetch CSRF token
-    const csrfResponse = await axios.get(`${updatedUrl}/sanctum/csrf-cookie`, { withCredentials: true });
-    const sanctumCsrfToken = csrfResponse.data.csrfToken
-    console.log(sanctumCsrfToken);
+    await axios.get(`${process.env.REACT_APP_BASE_URL}/sanctum/csrf-cookie`, { withCredentials: true });
     
-    // Log the CSRF response to debug
-    console.log('CSRF Response:', csrfResponse);
-
-    // Extract CSRF token from cookies
-    const csrfToken = getCsrfTokenFromCookies();
-    if (!csrfToken) {
-      throw new Error('CSRF token not found');
-    }
-    console.log('Fetched CSRF Token:', csrfToken);
-
-    // Make the login request with the CSRF token
-    const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, credentials, {
+    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/login`, credentials, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        // 'X-CSRF-TOKEN': csrfResponse.data.csrfToken,
-        'X-CSRF-TOKEN': csrfToken,
       },
-      withCredentials: true // Ensure cookies are sent
+      withCredentials: true, // Ensure cookies are sent,
+      withXSRFToken:true
     });
     
-    const { fullName, avatar, token } = response.data;
+    const { token } = response.data;
     console.log('Login token: ', token);
+    dispatch(login({ token }));
     
-    dispatch(login({ fullName, avatar, token }));
     localStorage.setItem('auth', JSON.stringify({
       loggedIn: true,
-      fullName,
-      avatar,
       token,
     }));
     
     // Return the response to handle in the component
     return { payload: { token } };
-    
   } catch (error) {
     console.error('Login failed:', error);
     dispatch(handleAuthError('Login failed. Check your credentials &  please try again.'));
@@ -91,14 +90,36 @@ export const handleLogin = credentials => async (dispatch) => {
   }
 };
 
-// Helper function to extract CSRF token from cookies
-const getCsrfTokenFromCookies = () => {
-  const match = document.cookie.match(/XSRF-TOKEN=([^;]*)/);
-  return match ? decodeURIComponent(match[1]) : null;
-};
-
 export const handleAuthError = error => (dispatch) => {
   dispatch(authError(error));
+};
+
+export const handleRegister = ({
+ username, email, password, password_confirmation,
+ }) => async (dispatch) => {
+  try {
+    await axios.get(`${process.env.REACT_APP_BASE_URL}/sanctum/csrf-cookie`);
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/register`, {
+      username,
+      email,
+      password,
+      password_confirmation,
+    }, {
+
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+
+    const { token } = response.data;
+    console.log(token)
+    dispatch(registerSuccess(token));
+    return response;
+  } catch (error) {
+    console.error('Registration failed:', error);
+    dispatch(registerError(error));
+    throw error.response.data;
+  }
 };
 
 export function registerSuccess(token) {
@@ -114,35 +135,3 @@ export function registerError(error) {
     error,
   };
 }
-
-export const handleRegister = ({
- username, email, password, password_confirmation,
- }) => async (dispatch) => {
-  try {
-    const updatedUrl = process.env.REACT_APP_API_URL.replace(/\/api/, '');
-    await axios.get(`${updatedUrl}/sanctum/csrf-cookie`);
-    const response = await axios.post(`${process.env.REACT_APP_API_URL}/register`, {
-        username,
-        email,
-        password,
-        password_confirmation,
-    }, {
-        headers: {
-            Accept: 'application/json',
-        },
-    });
-
-    const { token } = response.data;
-    console.log(token)
-    dispatch(registerSuccess(token));
-    localStorage.setItem('auth', JSON.stringify({
-      loggedIn: true,
-      token,
-    }));
-    return response;
-  } catch (error) {
-    console.error('Registration failed:', error);
-    dispatch(registerError(error));
-    throw error.response.data;
-  }
-};
