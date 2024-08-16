daededa2afda2c6ddf6a4ba5fb4f33f75bcba90d
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-16 19:10:32 +0300

    Use Outlet for nested Routes

--- src/containers/App/Router/WrappedRoutes/Charts.jsx
+++ src/containers/App/Router/WrappedRoutes/Charts.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import ChartsJS from '@/containers/Charts/ChartJs';
-import ReactVis from '@/containers/Charts/ReactVis';
-import Recharts from '@/containers/Charts/Recharts';
-import Amcharts from '@/containers/Charts/Amcharts';
+import { Outlet } from 'react-router-dom';
 
-export default () => (
-  <Switch>
-    <Route path="/charts/charts_js" component={ChartsJS} />
-    <Route path="/charts/react_vis" component={ReactVis} />
-    <Route path="/charts/recharts" component={Recharts} />
-    <Route path="/charts/amcharts" component={Amcharts} />
-  </Switch>
+const Charts = () => (
+  <div>
+    <Outlet />
+  </div>
 );
+
+export default Charts;
