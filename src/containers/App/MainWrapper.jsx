87180c400bed9d264d431db0092859963a5b0dae
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-16 19:10:16 +0300

    Update in MainWrapper after updates

--- src/containers/App/MainWrapper.jsx
+++ src/containers/App/MainWrapper.jsx
@@ -1,42 +1,40 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAppConfig } from '@/redux/actions/appConfigActions';
 import Loading from '@/shared/components/Loading';
 
 const direction = (location, rtl) => (location.pathname === '/' ? 'ltr' : rtl.direction);
 
-const MainWrapper = ({ children, location }) => {
+const MainWrapper = ({ children }) => {
   const { rtl, isFetching } = useSelector(state => ({
     rtl: state.rtl,
     isFetching: state.appConfig.isFetching,
   }));
 
   const dispatch = useDispatch();
-  
+  const location = useLocation();
+
   useEffect(() => {
     dispatch(fetchAppConfig());
   }, [dispatch]);
 
   return isFetching ? (
     <Loading loading={isFetching} />
-      ) : (
-        <div
-          className={`${direction(location, rtl)}-support`}
-        >
-          <div className="wrapper">
-            {children}
-          </div>
-        </div>
-      );
+  ) : (
+    <div className={`${direction(location.pathname, rtl)}-support`}>
+      <div className="wrapper">
+        {children}
+      </div>
+    </div>
+  );
 };
+
 
 MainWrapper.propTypes = {
   children: PropTypes.node.isRequired,
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
-  }).isRequired,
 };
 
-export default withRouter(MainWrapper);
+
+export default MainWrapper;
