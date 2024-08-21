c9c6e7d717e6751e7198cefe454e402deaf7f67e
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-21 12:39:47 +0300

    Fix Documentation to appear from menu

--- src/containers/App/Router/WrappedRoutes/Documentation.jsx
+++ src/containers/App/Router/WrappedRoutes/Documentation.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Introduction from '../../../Documentation/01_introduction/index';
-import Installation from '../../../Documentation/02_installation/index';
-import FileStructure from '../../../Documentation/03_files_structure/index';
-import Components from '../../../Documentation/04_components/index';
-import Form from '../../../Documentation/06_forms/index';
-import ColorThemes from '../../../Documentation/07_change_and_add_color_themes/index';
-import NavigationItem from '../../../Documentation/08_new_navigation_item/index';
-import Resources from '../../../Documentation/10_resources/index';
-import Changelog from '../../../Documentation/11_changelog/index';
-import FAQ from '../../../Documentation/12_faq/index';
+import { Route, Routes, Outlet } from 'react-router-dom';
 
-export default () => (
-  <Routes>
-    <Route path="/documentation/introduction" element={<Introduction />} />
-    <Route path="/documentation/installation" element={<Installation />} />
-    <Route path="/documentation/file_structure" element={<FileStructure />} />
-    <Route path="/documentation/components" element={<Components />} />
-    <Route path="/documentation/form" element={<Form />} />
-    <Route path="/documentation/color_themes" element={<ColorThemes />} />
-    <Route path="/documentation/navigation_item" element={<NavigationItem />} />
-    <Route path="/documentation/resources" element={<Resources />} />
-    <Route path="/documentation/changelog" element={<Changelog />} />
-    <Route path="/documentation/faq" element={<FAQ />} />
-  </Routes>
+
+const Documentation = () => (
+    <div>
+      <Outlet />
+    </div>
 );
+
+export default Documentation;
