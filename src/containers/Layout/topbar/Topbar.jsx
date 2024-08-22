ccc177ddd09b4b9f1725aebf1ddf9f1c667b37b1
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-23 01:47:15 +0300

    Update logo to have conditional links

--- src/containers/Layout/topbar/Topbar.jsx
+++ src/containers/Layout/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TopbarMail from '../components/topbar/TopbarMail';
 import TopbarProfile from '../components/topbar/TopbarProfile';
@@ -17,25 +18,38 @@ import {
 const Topbar = ({
   changeMobileSidebarVisibility,
   changeSidebarVisibility,
-}) => (
-  <TopbarContainer>
-    <TopbarLeft>
-      <TopbarSidebarButton
-        onClickMobile={changeMobileSidebarVisibility}
-        onClickDesktop={changeSidebarVisibility}
-      />
-      <TopbarLogo to="/" />
-    </TopbarLeft>
-    <TopbarRight>
-      <TopbarSearchWrap>
-        <TopbarSearch />
-      </TopbarSearchWrap>
-      <TopbarRightOver>
-        <TopbarProfile />
-      </TopbarRightOver>
-    </TopbarRight>
-  </TopbarContainer>
+}) => {
+  const navigate = useNavigate();
+  const auth = JSON.parse(localStorage.getItem('auth'));
+
+  const handleLogoClick = () => {
+      if (auth && auth.loggedIn) {
+        navigate('/app_dashboard');
+      } else {
+        navigate('/login');
+      }
+  };
+ 
+  return(
+    <TopbarContainer>
+      <TopbarLeft>
+        <TopbarSidebarButton
+          onClickMobile={changeMobileSidebarVisibility}
+          onClickDesktop={changeSidebarVisibility}
+        />
+        <TopbarLogo to={auth && auth.loggedIn ? "/app_dashboard" : "/"} onClick={handleLogoClick}/>
+      </TopbarLeft>
+      <TopbarRight>
+        <TopbarSearchWrap>
+          <TopbarSearch />
+        </TopbarSearchWrap>
+        <TopbarRightOver>
+          <TopbarProfile />
+        </TopbarRightOver>
+      </TopbarRight>
+    </TopbarContainer>
   );
+};
 
 Topbar.propTypes = {
   changeMobileSidebarVisibility: PropTypes.func.isRequired,
