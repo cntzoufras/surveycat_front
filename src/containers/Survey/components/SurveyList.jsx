0532681f1b4ed2a676a25f55199e7e98c0da2db5
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-24 18:49:45 +0300

    Add SurveyList component

--- src/containers/Survey/components/SurveyList.jsx
+++ src/containers/Survey/components/SurveyList.jsx
@@ -4,8 +4,18 @@ import {
   Typography as MuiTypography,
   Grid as MuiGrid,
   Paper as MuiPaper,
+  Button as MuiButton,
+  Link as MuiLink,
 } from '@mui/material';
-import { getSurveys } from '@/utils/api/survey-api';
+import { getUserSurveys } from '@/utils/api/survey-api';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Create a theme with Roboto font
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
 
 const SurveyList = () => {
   const [surveys, setSurveys] = useState([]);
@@ -13,7 +23,7 @@ const SurveyList = () => {
   useEffect(() => {
     const fetchSurveys = async () => {
       try {
-        const response = await getSurveys();
+        const response = await getUserSurveys();
         if (response.ok) {
           setSurveys(response.data);
         } else {
@@ -27,26 +37,47 @@ const SurveyList = () => {
   }, []);
 
   return (
-    <MuiBox p={3}>
-      <MuiTypography variant="h4" gutterBottom>
-        Surveys
-      </MuiTypography>
-      <MuiGrid container spacing={3}>
-        {surveys.map(survey => (
-          <MuiGrid item xs={12} md={6} lg={4} key={survey.id}>
-            <MuiPaper elevation={3} style={{ padding: '16px' }}>
-              <MuiTypography variant="h6">{survey.title}</MuiTypography>
-              <MuiTypography variant="body2">Style: {survey.style}</MuiTypography>
-              <MuiBox mt={2}>
-                <MuiTypography variant="subtitle2">Number of Pages: {survey.pages.length}</MuiTypography>
-              </MuiBox>
-            </MuiPaper>
+    <ThemeProvider theme={theme}>
+      <MuiBox p={3} textAlign="center">
+        <MuiTypography variant="h4" gutterBottom>
+          Surveys
+        </MuiTypography>
+        {surveys.length === 0 ? (
+          <MuiBox mt={4}>
+            <MuiTypography variant="h6" gutterBottom>
+              No surveys yet.
+            </MuiTypography>
+            <MuiButton
+              variant="contained"
+              color="primary"
+              component={MuiLink}
+              href="/survey-design"
+            >
+              Create a new survey
+            </MuiButton>
+          </MuiBox>
+        ) : (
+          <MuiGrid container spacing={3}>
+            {surveys.map((survey) => (
+              <MuiGrid item xs={12} md={6} lg={4} key={survey.id}>
+                <MuiPaper elevation={3} style={{ padding: '16px' }}>
+                  <MuiTypography variant="h6">{survey.title}</MuiTypography>
+                  <MuiTypography variant="body2">
+                    Style: {survey.style}
+                  </MuiTypography>
+                  <MuiBox mt={2}>
+                    <MuiTypography variant="subtitle2">
+                      Number of Pages: {survey.pages.length}
+                    </MuiTypography>
+                  </MuiBox>
+                </MuiPaper>
+              </MuiGrid>
+            ))}
           </MuiGrid>
-        ))}
-      </MuiGrid>
-    </MuiBox>
+        )}
+      </MuiBox>
+    </ThemeProvider>
   );
 };
 
 export default SurveyList;
-
