918e2662f8240e552cab780dbbe1436c32291425
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-25 14:28:48 +0300

    Add some logging

--- src/containers/Survey/index.jsx
+++ src/containers/Survey/index.jsx
@@ -22,7 +22,7 @@ const SurveyPageWrapper = () => {
   const { surveyId, surveyPageId } = useParams();
   const { user, error, loggedIn } = useSelector(state => state.auth);
   const userId = user?.id;
-  
+  console.log(`Survey/index Survey ID: ${surveyId}, surveyPageID: ${surveyPageId}`)
   useEffect(() => {
     if (!userId) {
       console.warn('... refreshing page.');
@@ -51,6 +51,6 @@ const SurveyDesign = () => (
   <div>
     <SurveyPageWrapper />
   </div>
-  );
+);
 
 export default SurveyDesign;
