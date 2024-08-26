5d3fdcd284ac3f8b04035d725fd046edac6d9802
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-26 23:55:21 +0300

    Updated surveyActions & reducer for working SurveyPage, remove hooks

--- src/redux/actions/surveyActions.js
+++ src/redux/actions/surveyActions.js
@@ -1,4 +1,4 @@
-import  api from '@/utils/api/survey-api';
+import api from '@/utils/api/survey-api';
 
 export const CREATE_SURVEY_QUESTION_SUCCESS = 'CREATE_SURVEY_QUESTION_SUCCESS';
 export const CREATE_SURVEY_QUESTION_FAIL = 'CREATE_SURVEY_QUESTION_FAIL';
@@ -9,7 +9,10 @@ export const ADD_SURVEY_PAGE_SUCCESS = 'ADD_SURVEY_PAGE_SUCCESS';
 export const ADD_SURVEY_PAGE_FAIL = 'ADD_SURVEY_PAGE_FAIL';
 
 export const FETCH_SURVEY_QUESTIONS = 'FETCH_SURVEY_QUESTIONS';
-export const FETCH_SURVEY_PAGES = 'FETCH_SURVEY_PAGES'
+export const FETCH_SURVEY_PAGES = 'FETCH_SURVEY_PAGES';
+
+export const FETCH_STOCK_SURVEYS_SUCCESS = 'FETCH_STOCK_SURVEYS_SUCCESS';
+export const FETCH_STOCK_SURVEYS_FAIL = 'FETCH_STOCK_SURVEYS_FAIL';
 
 export const UPDATE_SURVEY_TITLE_SUCCESS = 'UPDATE_SURVEY_TITLE_SUCCESS';
 export const UPDATE_SURVEY_TITLE_FAIL = 'UPDATE_SURVEY_TITLE_FAIL';
@@ -21,14 +24,17 @@ export const UPDATE_SURVEY_PAGE_TITLE_FAIL = 'UPDATE_SURVEY_PAGE_TITLE_FAIL';
 export const UPDATE_SURVEY_PAGE_DESCRIPTION_SUCCESS = 'UPDATE_SURVEY_PAGE_DESCRIPTION_SUCCESS';
 export const UPDATE_SURVEY_PAGE_DESCRIPTION_FAIL = 'UPDATE_SURVEY_PAGE_DESCRIPTION_FAIL';
 
-export const UPDATE_SURVEY_LAYOUT_SUCCESS = 'UPDATE_SURVEY_LAYOUT_SUCCESS'
-export const UPDATE_SURVEY_LAYOUT_FAIL = 'UPDATE_SURVEY_LAYOUT_FAIL'
+export const UPDATE_SURVEY_LAYOUT_SUCCESS = 'UPDATE_SURVEY_LAYOUT_SUCCESS';
+export const UPDATE_SURVEY_LAYOUT_FAIL = 'UPDATE_SURVEY_LAYOUT_FAIL';
+
+export const DELETE_SURVEY_QUESTION_SUCCESS = 'DELETE_SURVEY_QUESTION_SUCCESS';
+export const DELETE_SURVEY_QUESTION_FAIL = 'DELETE_SURVEY_QUESTION_FAIL';
 
 
-
+// Action to add a new survey page
 export const addSurveyPageAction = (surveyId, newPageData) => async (dispatch) => {
   try {
-    const response = await createSurveyPage(newPageData);
+    const response = await api.post(`/survey-pages`, newPageData);
     dispatch({ type: ADD_SURVEY_PAGE_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
@@ -37,70 +43,96 @@ export const addSurveyPageAction = (surveyId, newPageData) => async (dispatch) =
   }
 };
 
-export const createSurveyQuestion = questionData => async (dispatch) => {
+// Action to fetch survey questions
+export const fetchSurveyQuestionsAction = (surveyId, surveyPageId) => async (dispatch) => {
+  try {
+    const response = await api.get(`/surveys/${surveyId}/pages/${surveyPageId}/questions`);
+    dispatch({
+      type: FETCH_SURVEY_QUESTIONS,
+      payload: response.data, // Assuming response.data contains the list of questions
+    });
+  } catch (error) {
+    console.error('Error fetching survey questions:', error);
+    // You can also dispatch an error action if needed
+  }
+};
+
+export const fetchStockSurveysAction = () => async (dispatch) => {
+  try {
+    const response = await api.get('/surveys/stock');
+    console.log(`Stock Surveys apo surveyAction response me data einai: `, response.data.data);
+    dispatch({ type: FETCH_STOCK_SURVEYS_SUCCESS, payload: response.data.data });
+  } catch (error) {
+    dispatch({ type: FETCH_STOCK_SURVEYS_FAIL, payload: error.message });
+    throw error;
+  }
+};
+
+// Action to create a new survey question
+export const createSurveyQuestionAction = (questionData) => async (dispatch) => {
   try {
     const response = await api.post('/survey-questions', questionData);
     dispatch({ type: CREATE_SURVEY_QUESTION_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: CREATE_SURVEY_QUESTION_FAIL, payload: error });
+    dispatch({ type: CREATE_SURVEY_QUESTION_FAIL, payload: error.message });
     throw error;
   }
 };
 
-export const createSurveyQuestionChoices = (choicesData) => async (dispatch) => {
+// Action to delete a survey question
+export const deleteSurveyQuestionAction = (surveyQuestionId) => async (dispatch) => {
+  try {
+    await api.delete(`/survey-questions/${surveyQuestionId}`);
+    dispatch({ type: DELETE_SURVEY_QUESTION_SUCCESS, payload: surveyQuestionId });
+  } catch (error) {
+    dispatch({ type: DELETE_SURVEY_QUESTION_FAIL, payload: error.message });
+    throw error;
+  }
+};
+
+// Action to create survey question choices
+export const createSurveyQuestionChoicesAction = (choicesData) => async (dispatch) => {
   try {
     const response = await api.post('/survey-question-choices', choicesData);
     dispatch({ type: CREATE_SURVEY_QUESTION_CHOICES_SUCCESS, payload: response.data });
   } catch (error) {
-    dispatch({ type: CREATE_SURVEY_QUESTION_CHOICES_FAIL, payload: error });
+    dispatch({ type: CREATE_SURVEY_QUESTION_CHOICES_FAIL, payload: error.message });
     throw error;
   }
 };
 
-export const fetchSurveyQuestionsAction = (surveyId, surveyPageId) => async (dispatch) => {
-    try {
-        const response = await api.get(`/surveys/${surveyId}/pages/${surveyPageId}/questions`);
-        
-        dispatch({
-            type: FETCH_SURVEY_QUESTIONS,
-            payload: response.data, // Assuming response.data contains the list of questions
-        });
-    } catch (error) {
-        console.error('Error fetching survey questions:', error);
-        // You can also dispatch an error action if needed
-    }
-};
-
+// Action to update survey title
 export const updateSurveyTitleAction = (surveyId, title) => async (dispatch) => {
   try {
     const response = await api.put(`/surveys/${surveyId}`, { title });
     dispatch({ type: UPDATE_SURVEY_TITLE_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: UPDATE_SURVEY_TITLE_FAIL, payload: error });
+    dispatch({ type: UPDATE_SURVEY_TITLE_FAIL, payload: error.message });
     throw error;
   }
 };
-
+// Action to update survey description
 export const updateSurveyDescriptionAction = (surveyId, description) => async (dispatch) => {
   try {
     const response = await api.put(`/surveys/${surveyId}`, { description });
     dispatch({ type: UPDATE_SURVEY_DESCRIPTION_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: UPDATE_SURVEY__DESCRIPTION_FAIL, payload: error });
+    dispatch({ type: UPDATE_SURVEY_DESCRIPTION_FAIL, payload: error.message });
     throw error;
   }
 };
 
+// Action to update survey page title
 export const updateSurveyPageTitleAction = (surveyPageId, title) => async (dispatch) => {
   try {
     const response = await api.put(`/survey-pages/${surveyPageId}`, { title });
-    dispatch({ type: UPDATE_SURVEY_TITLE_SUCCESS, payload: response.data });
+    dispatch({ type: UPDATE_SURVEY_PAGE_TITLE_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: UPDATE_SURVEY_TITLE_FAIL, payload: error });
+    dispatch({ type: UPDATE_SURVEY_PAGE_TITLE_FAIL, payload: error.message });
     throw error;
   }
 };
@@ -108,21 +140,22 @@ export const updateSurveyPageTitleAction = (surveyPageId, title) => async (dispa
 export const updateSurveyPageDescriptionAction = (surveyPageId, description) => async (dispatch) => {
   try {
     const response = await api.put(`/survey-pages/${surveyPageId}`, { description });
-    dispatch({ type: UPDATE_SURVEY_DESCRIPTION_SUCCESS, payload: response.data });
+    dispatch({ type: UPDATE_SURVEY_PAGE_DESCRIPTION_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: UPDATE_SURVEY_DESCRIPTION_FAIL, payload: error });
+    dispatch({ type: UPDATE_SURVEY_PAGE_DESCRIPTION_FAIL, payload: error.message });
     throw error;
   }
 };
 
+// Action to update survey layout
 export const updateSurveyLayoutAction = (surveyId, layout) => async (dispatch) => {
   try {
     const response = await api.put(`/surveys/${surveyId}`, { layout });
     dispatch({ type: UPDATE_SURVEY_LAYOUT_SUCCESS, payload: response.data });
     return response.data;
   } catch (error) {
-    dispatch({ type: UPDATE_SURVEY_LAYOUT_FAIL, payload: error });
+    dispatch({ type: UPDATE_SURVEY_LAYOUT_FAIL, payload: error.message });
     throw error;
   }
 };
--- src/redux/reducers/surveyReducer.js
+++ src/redux/reducers/surveyReducer.js
@@ -19,41 +19,26 @@ import {
   UPDATE_SURVEY_LAYOUT_FAIL,
   ADD_SURVEY_PAGE_SUCCESS,
   ADD_SURVEY_PAGE_FAIL,
+  DELETE_SURVEY_QUESTION_SUCCESS,
+  DELETE_SURVEY_QUESTION_FAIL,
+  FETCH_STOCK_SURVEYS_SUCCESS,
+  FETCH_STOCK_SURVEYS_FAIL
 } from '../actions/surveyActions';
 
 const initialState = {
-  questions: [],
-  choices: [],
-  loading: false,
-  error: null,
+  survey: null, // Holds the current survey's main details like title, description, etc.
+  surveyPage: null, // Holds the current survey page details like title, description, etc.
+  surveyPages: [], // Holds all pages related to a survey
+  stockSurveys: [], // Add stockSurveys to the initial state
+  questions: [], // Holds questions related to the current survey page
+  choices: [], // Holds the choices related to the current questions
+  layout: 'default', // Tracks layout settings for the survey
+  loading: false, // Indicates if any async operation is in progress
+  error: null, // Holds any error messages encountered during operations
 };
 
 const surveyReducer = (state = initialState, action) => {
   switch (action.type) {
-    case CREATE_SURVEY_QUESTION_SUCCESS:
-      return {
-        ...state,
-        questions: [...state.questions, action.payload],
-        loading: false,
-      };
-    case CREATE_SURVEY_QUESTION_FAIL:
-      return {
-        ...state,
-        error: action.payload,
-        loading: false,
-      };
-    case CREATE_SURVEY_QUESTION_CHOICES_SUCCESS:
-      return {
-        ...state,
-        choices: [...state.choices, ...action.payload],
-        loading: false,
-      };
-    case CREATE_SURVEY_QUESTION_CHOICES_FAIL:
-      return {
-        ...state,
-        error: action.payload,
-        loading: false,
-      };
     case ADD_SURVEY_PAGE_SUCCESS:
       return {
         ...state,
@@ -66,8 +51,50 @@ const surveyReducer = (state = initialState, action) => {
       };
     case FETCH_SURVEY_QUESTIONS:
       return {
-          ...state,
-          questions: action.payload,
+        ...state,
+        questions: action.payload,
+      };
+    case FETCH_STOCK_SURVEYS_SUCCESS:
+      return {
+        ...state,
+        stockSurveys: action.payload,
+        loading: false,
+      };
+    case FETCH_STOCK_SURVEYS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case CREATE_SURVEY_QUESTION_SUCCESS:
+      return {
+        ...state,
+        questions: [...state.questions, action.payload],
+      };
+    case CREATE_SURVEY_QUESTION_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case DELETE_SURVEY_QUESTION_SUCCESS:
+      return {
+        ...state,
+        questions: state.questions.filter(q => q.id !== action.payload),
+      };
+    case DELETE_SURVEY_QUESTION_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case CREATE_SURVEY_QUESTION_CHOICES_SUCCESS:
+      return {
+        ...state,
+        choices: [...state.choices, ...action.payload],
+      };
+    case CREATE_SURVEY_QUESTION_CHOICES_FAIL:
+      return {
+        ...state,
+        error: action.payload,
       };
     case UPDATE_SURVEY_TITLE_SUCCESS:
       return {
@@ -79,15 +106,45 @@ const surveyReducer = (state = initialState, action) => {
         ...state,
         error: action.payload,
       };
+    case UPDATE_SURVEY_DESCRIPTION_SUCCESS:
+      return {
+        ...state,
+        survey: { ...state.survey, description: action.payload.description },
+      };
+    case UPDATE_SURVEY_DESCRIPTION_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
     case UPDATE_SURVEY_PAGE_TITLE_SUCCESS:
       return {
         ...state,
         surveyPage: { ...state.surveyPage, title: action.payload.title },
       };
+    case UPDATE_SURVEY_PAGE_TITLE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
     case UPDATE_SURVEY_PAGE_DESCRIPTION_SUCCESS:
       return {
         ...state,
         surveyPage: { ...state.surveyPage, description: action.payload.description },
+      };
+    case UPDATE_SURVEY_PAGE_DESCRIPTION_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case UPDATE_SURVEY_LAYOUT_SUCCESS:
+      return {
+        ...state,
+        layout: action.payload.layout, 
+      };
+    case UPDATE_SURVEY_LAYOUT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
       };
     case 'LOADING':
       return {
