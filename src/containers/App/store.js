ba5174c240a91a4edd8b9bd32a217de2503b8754
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-26 23:56:14 +0300

    [WIP] Test fixes for redux dev tools

--- src/containers/App/store.js
+++ src/containers/App/store.js
@@ -1,5 +1,6 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware, } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import {
   themeReducer,
   rtlReducer,
