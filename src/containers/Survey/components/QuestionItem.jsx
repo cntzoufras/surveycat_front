5c3de4c3ac3b37ac0d9f43b00b452982309879df
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-28 21:40:28 +0300

    Add Question Types in Question item to adjust for ui

--- src/containers/Survey/components/QuestionItem.jsx
+++ src/containers/Survey/components/QuestionItem.jsx
@@ -9,8 +9,6 @@ import {
   FormControlLabel,
   List,
   ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -21,32 +19,40 @@ const QuestionItem = ({
   const questionType = question.question_type_id || 'unknown type';
   const questionPage = question.survey_page_id || 'No page info';
 
+  // A mapping of question type IDs to their names, for better readability
+  const questionTypeNames = {
+    1: 'Multiple Choice (Radio)',
+    2: 'Checkboxes',
+    3: 'Star Rating',
+    4: 'Best Worst Scale',
+    5: 'Single Textbox',
+    6: 'Comment Box',
+    7: 'Dropdown',
+    8: 'Ranking',
+    9: 'Slider',
+    10: 'Multiple Checkboxes',
+    11: 'Date/Time',
+  };
+
   return (
     <Box key={question.id} sx={{ mb: 2, p: 2, border: '1px solid #ccc', borderRadius: '8px' }}>
       <Typography variant="h6">
-        {`${index + 1}. ${questionTitle} (${questionType}) (${questionPage})`}
+        {`${index + 1}. ${questionTitle} (${questionTypeNames[questionType]}) (${questionPage})`}
       </Typography>
       
-      {['radio', 'checkbox'].includes(questionType) && question.options && (
+      {questionType === 1 && question.options && (
         <List>
           {question.options.map((option, optIndex) => (
             <ListItem key={`${question.id}-${optIndex}`} disablePadding>
               <FormControlLabel
-                control={questionType === 'radio' ? (
+                control={
                   <Radio
                     checked={question.selectedOption === option}
                     onChange={() => onOptionSelection(question.id, option)}
                     name={`question-${index}`}
                     value={option}
                   />
-                ) : (
-                  <Checkbox
-                    checked={question.selectedOption === option}
-                    onChange={() => onOptionSelection(question.id, option)}
-                    name={`question-${index}`}
-                    value={option}
-                  />
-                )}
+                }
                 label={option}
               />
             </ListItem>
@@ -54,6 +60,28 @@ const QuestionItem = ({
         </List>
       )}
 
+      {questionType === 2 && question.options && (
+        <List>
+          {question.options.map((option, optIndex) => (
+            <ListItem key={`${question.id}-${optIndex}`} disablePadding>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={question.selectedOption === option}
+                    onChange={() => onOptionSelection(question.id, option)}
+                    name={`question-${index}`}
+                    value={option}
+                  />
+                }
+                label={option}
+              />
+            </ListItem>
+          ))}
+        </List>
+      )}
+
+      {/* Add similar blocks for other question types, if needed */}
+      
       {onDelete && (
         <IconButton edge="end" aria-label="delete" onClick={() => onDelete(question.id)}>
           <DeleteIcon />
@@ -67,7 +95,7 @@ QuestionItem.propTypes = {
   question: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string,
-    question_type_id: PropTypes.number,
+    question_type_id: PropTypes.number.isRequired,
     surveyPage: PropTypes.string,
     options: PropTypes.arrayOf(PropTypes.string),
     selectedOption: PropTypes.string,
