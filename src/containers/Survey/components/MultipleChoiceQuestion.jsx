a977cc67d4ee3f3ae12e875c3a25aa9e8d82e42e
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-29 13:05:15 +0300

    Split add question modal elements in simple components

---
+++ src/containers/Survey/components/MultipleChoiceQuestion.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import OptionInputList from './OptionInputList';
+
+const MultipleChoiceQuestion = ({ numOptions, optionInputs, handleOptionInputChange, handleNumOptionsChange }) => {
+  return (
+    <>
+      <Form.Group controlId="formNumOptions">
+        <Form.Label>Number of Options</Form.Label>
+        <Form.Control
+          type="number"
+          value={numOptions}
+          onChange={handleNumOptionsChange}
+        />
+      </Form.Group>
+      <OptionInputList
+        questionType="radio"
+        numOptions={numOptions}
+        optionInputs={optionInputs}
+        handleOptionInputChange={handleOptionInputChange}
+      />
+    </>
+  );
+};
+
+MultipleChoiceQuestion.propTypes = {
+  numOptions: PropTypes.number.isRequired,
+  optionInputs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleOptionInputChange: PropTypes.func.isRequired,
+  handleNumOptionsChange: PropTypes.func.isRequired,
+};
+
+export default MultipleChoiceQuestion;
---
+++ src/containers/Survey/components/StarRatingQuestion.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+
+const StarRatingQuestion = ({ handleQuestionInputChange, newQuestion }) => {
+  return (
+    <Form.Group controlId="formStarRating">
+      <Form.Label>Star Rating</Form.Label>
+      <Form.Control
+        type="text"
+        value={newQuestion}
+        onChange={handleQuestionInputChange}
+        placeholder="Enter a description for the rating"
+      />
+    </Form.Group>
+  );
+};
+
+StarRatingQuestion.propTypes = {
+  newQuestion: PropTypes.string.isRequired,
+  handleQuestionInputChange: PropTypes.func.isRequired,
+};
+
+export default StarRatingQuestion;
