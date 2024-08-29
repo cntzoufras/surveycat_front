10dcea698eaba7426d770113242ca64ae92d62de
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-08-30 00:21:31 +0300

    Remove deprecated test

--- src/containers/App/store.js
+++ src/containers/App/store.js
@@ -1,6 +1,5 @@
 import { combineReducers, createStore, applyMiddleware, } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import {
   themeReducer,
   rtlReducer,
