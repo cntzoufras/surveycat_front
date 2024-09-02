dc0d65ac20bd356eb434a8bfa97cb530199b816c
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-09-02 19:16:23 +0300

    Fix yarn lint errors

--- src/containers/Survey/components/VerificationAlready.jsx
+++ src/containers/Survey/components/VerificationAlready.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const VerificationAlready = () => (
   <div style={{ textAlign: 'center', marginTop: '40vh' }}>
     <h1 style={{ color: '#252525' }}>Email Already Verified</h1>
-    <p style={{ color: '#252525' }}>Your email has already been verified. You can now log in or continue using the app.</p>
+    <p style={{ color: '#252525' }}>
+      Your email has already been verified. You can now log in or continue using the app.
+    </p>
   </div>
 );
 
--- src/containers/Survey/components/VerificationSuccess.jsx
+++ src/containers/Survey/components/VerificationSuccess.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const VerificationSuccess = () => (
   <div style={{ textAlign: 'center', marginTop: '40vh' }}>
     <h1 style={{ color: '#252525' }}>Email Verified Successfully</h1>
-    <p style={{ color: '#252525' }}>Your email has been successfully verified. You can now log in or continue using the app.</p>
+    <p style={{ color: '#252525' }}>
+      Your email has been successfully verified. 
+      You can now log in or continue using the app.
+    </p>
   </div>
 );
 
