43d28964bcd79603fcbece39fd3e3ec66af16190
Constantinos Ntzoufras <cntzoufras@gmail.com>
2024-09-26 21:24:20 +0300

    Update Respondents index & add CreateData

---
+++ src/containers/Dashboards/Surveys/Respondents/index.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { fetchRespondentsAction } from '@/redux/actions/respondentsActions';
+import RespondentsReactTable from './components/RespondentsReactTable';
+
+const Respondents = () => {
+  const { t } = useTranslation('common');
+  const dispatch = useDispatch();
+  
+  const { respondents = [], loading, error } = useSelector(state => state.respondents || {});
+
+  useEffect(() => {
+    dispatch(fetchRespondentsAction());
+  }, [dispatch]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: '#',
+        accessor: 'id',
+        disableGlobalFilter: true,
+        width: 65,
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+      },
+      {
+        Header: 'Age',
+        accessor: 'age',
+        disableGlobalFilter: true,
+      },
+      {
+        Header: 'Gender',
+        accessor: 'gender',
+        disableGlobalFilter: true,
+      },
+      {
+        Header: 'Location',
+        accessor: 'location',
+        disableGlobalFilter: true,
+      },
+      {
+        Header: 'Date Created',
+        accessor: 'created_at',
+        disableGlobalFilter: true,
+      },
+    ],
+    [],
+  );
+
+  // Helper function to parse details JSON
+  const parseDetails = (details) => {
+    try {
+      return JSON.parse(details);
+    } catch (e) {
+      return { age: null, gender: null, location: null };
+    }
+  };
+
+  const data = respondents.data?.map((respondent, index) => {
+    const { age, gender, location } = parseDetails(respondent.details);
+    return {
+      id: index + 1,
+      email: respondent.email || 'N/A',
+      age,
+      gender,
+      location,
+      created_at: new Date(respondent.created_at).toLocaleDateString(),
+    };
+  }) || [];
+
+  const reactTableData = { tableHeaderData: columns, tableRowsData: data };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>
+          <h3 className="page-title">{t('respondents.title')}</h3>
+          <h3 className="page-subhead subhead">{t('respondents.description')}</h3>
+        </Col>
+      </Row>
+      <Row>
+        <RespondentsReactTable reactTableData={reactTableData} />
+      </Row>
+    </Container>
+  );
+};
+
+export default Respondents;
