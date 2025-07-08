3da827d23d13724b565165902b5d7d42740a00cc
Constantinos Ntzoufras <cntzoufras@gmail.com>
2025-07-09 00:49:31 +0300

    Persist theme across user session

--- src/redux/reducers/themeReducer.js
+++ src/redux/reducers/themeReducer.js
@@ -3,15 +3,19 @@ import {
   CHANGE_THEME_TO_LIGHT,
 } from '../actions/themeActions';
 
+const stored = localStorage.getItem('theme') || 'dark';
+
 const initialState = {
-  className: 'dark',
+  className: stored,
 };
 
 const themeReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_THEME_TO_DARK:
+      localStorage.setItem('theme', 'dark');
       return { className: 'dark' };
     case CHANGE_THEME_TO_LIGHT:
+      localStorage.setItem('theme', 'light');
       return { className: 'light' };
     default:
       return state;
