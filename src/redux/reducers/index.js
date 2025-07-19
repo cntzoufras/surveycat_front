f8b8db31a5939b7d346b3d8d72c2ebabced8cb09
Constantinos Ntzoufras <cntzoufras@gmail.com>
2025-07-19 03:11:42 +0300

    Implement cleanup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,27 +1,19 @@
 import themeReducer from './themeReducer';
-import rtlReducer from './rtlReducer';
 import roundBordersReducer from './roundBordersReducer';
 import blocksShadowsReducer from './blocksShadowsReducer';
 import sidebarReducer from './sidebarReducer';
-import newOrderTableReducer from './newOrderTableReducer';
 import customizerReducer from './customizerReducer';
-import todoReducer from './todoReducer';
 import authReducer from './authReducer';
-import pokemonReducer from './pokemonReducer';
 import surveyReducer from './surveyReducer';
 import userReducer from './userReducer';
 
 export {
   themeReducer,
-  rtlReducer,
   sidebarReducer,
-  newOrderTableReducer,
   customizerReducer,
   roundBordersReducer,
   blocksShadowsReducer,
-  todoReducer,
   authReducer,
-  pokemonReducer,
   surveyReducer,
   userReducer,
 };
