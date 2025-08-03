f5c45cefd4ec3e4e20d354a8cf17704f75c8739a
Constantinos Ntzoufras <cntzoufras@gmail.com>
2025-08-03 16:53:22 +0300

    Cleanup code

--- src/containers/Dashboards/App/index.jsx
+++ src/containers/Dashboards/App/index.jsx
@@ -4,12 +4,10 @@ import { Col, Container, Row } from 'react-bootstrap';
 import CurrentUsers from './components/CurrentUsers';
 import ActiveUsers from './components/ActiveUsers';
 import SessionShort from './components/SessionShort';
-import ActiveUsersShort from './components/ActiveUsersShort';
 import NewUsersShort from './components/NewUsersShort';
 import PageViewsShort from './components/PageViewsShort';
 import AppTileClicks from './components/AppTileClicks';
 import WeeklyStatMobile from './components/WeeklyStatMobile';
-import SocialMarketing from './components/SocialMarketing';
 
 const AppDashboard = () => {
   const { t } = useTranslation('common');
